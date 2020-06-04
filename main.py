aea647870d5c6091afb3655b34340dc9073322dc
arkady <larytet@yahoo.com>
2020-06-04 19:04:44 +0300

    Comment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ See  https://bugs.python.org/issue40860
 
 Try 
 
-    docker build --tag multiprocess . && docker run -e LOGLEVEL=DEBUG multiprocess
+    docker build --tag multiprocess . && docker run -e LOGLEVEL=INFO multiprocess
 """
 
 import logging
